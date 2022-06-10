7123d00be201461dc38efc9e1ed3a889f3c3943b
Gabriel <47752724+Botche@users.noreply.github.com>
2022-06-10 22:03:13 +0200

    Show the category on blog details

--- cats-blog/src/components/Blog/Details/index.jsx
+++ cats-blog/src/components/Blog/Details/index.jsx
@@ -1,27 +1,67 @@
 import { useParams, useNavigate } from 'react-router-dom';
 
-import useFetch from 'hooks/useFetch.jsx';
-
 import Spinner from 'components/Spinner';
 import Error from 'components/Error';
 
 import constants from 'utils/constants';
 import styles from './styles.module.scss';
 import Button from 'components/Button';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { generatePageTitle } from 'utils/helperFunctions';
+import messages from 'utils/messages';
 
 function Details() {
     const { id } = useParams();
     const navigate = useNavigate();
-    const blogDetailsUrl = constants.urls.blogUrl.replace(':id', id);
-    const { data: blog, error, isLoading } = useFetch(blogDetailsUrl);
+
+    const [blog, setBlog] = useState();
+    const [category, setCategory] = useState();
+    const [error, setError] = useState();
+    const [isLoading, setIsLoading] = useState();
 
     useEffect(() => {
-        if (blog) {
-            document.title = generatePageTitle(blog.title);
-        }
-    }, [blog]);
+        const blogDetailsUrl = constants.urls.blogUrl.replace(':id', id);
+        fetch(blogDetailsUrl, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then(response => {
+                if (response.ok === false) {
+                    throw Error(messages.errorFetchingResourse); 
+                }
+
+                return response.json();
+            })
+            .then(blog => {
+                document.title = generatePageTitle(blog.title);
+                const categoryUrl = constants.urls.categoryUrl.replace(':id', blog.id);
+                fetch(categoryUrl, {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                    .then(response => {
+                        if (response.ok === false) {
+                            throw Error(messages.errorFetchingResourse); 
+                        }
+        
+                        return response.json();
+                    })
+                    .then(category => {
+                        setError(null);
+                        setIsLoading(false);
+                        setBlog(blog);
+                        setCategory(category);
+                    });
+            })
+            .catch(error => {
+                setIsLoading(false);
+                setError(error.message);
+            });
+    }, [id]);
 
     const handleDelete = () => {
         const deleteBlogUrl = constants.urls.blogUrl.replace(':id', id);
@@ -45,6 +85,7 @@ function Details() {
             { blog && (
                 <article className={styles['blog-details']}>
                     <h1 className={styles['blog-details__heading']}>{blog.title}</h1>
+                    <p className={styles['blog-details__category']}>Category: {category.name}</p>
                     <p className={styles['blog-details__author']}>Written by: {blog.author}</p>
                     <div className={styles['blog-details__content']}>{blog.body}</div>
 
